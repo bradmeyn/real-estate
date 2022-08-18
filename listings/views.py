6f30acb6a4bcbb2923c10beaa65813dce90d3ec2
bradmeyn <brad@redcrowdigital.com.au>
2022-08-19 09:10:47 +1000

    start search page

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
+from listings.choices import bedroom_choices, state_choices, price_choices
 
 def index(request):
 
@@ -25,4 +26,10 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+    return render(request, 'listings/search.html', context)
