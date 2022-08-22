712c5550d6e1395dba62ac8e10b3daa8c565c8cc
bradmeyn <brad@redcrowdigital.com.au>
2022-08-23 09:02:19 +1000

    create login register routes

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,39 @@
+from email import message
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if passwords match
+        if password == password2:    
+        
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+
+        
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+
+        # Login User
+        
+        return 
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout():
     return redirect('index')
